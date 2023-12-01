bd071039b43d32a4994de60ef9b665d7aad896b7
Richelle <45215169+rcabatic@users.noreply.github.com>
2023-12-01 12:50:00 -0700

    changed structure of testing2

--- Oahu/testing_v2.py
+++ Oahu/testing_v2.py
@@ -7,23 +7,44 @@ Created on Thu Nov 30 13:31:56 2023
 
 import pandas as pd
 import numpy as np
-
+import matplotlib.pyplot as plt
 import os
 
 
-FILL_WITH_NANS = False
+# BEGINNING OF USER INPUT AREA
+
+flag_df_one_add_one = True
+# flag_df_one_add_one = False  # Use this for Kauai...
+
+region = 'EOahu'
+
+savefig = True
+# savefig = False
+
+# END OF USER INPUT AREA
+# ----------------------------------------------------------------------
+
 
 full_file_name = "EOahu_dat\Hauula_dat\HauulaToedist.txt"
 trunc_file_name = "EOahu_dat\Hauula_dat\HauulaTruncation.txt"
 
 
-## Load full file
-full = pd.read_csv(full_file_name, sep="\t", header=None).to_numpy()
+## Load full file with data (Toedist)
+full = pd.read_csv(full_file_name, sep="\t", header=None).to_numpy() 
+full_original = np.copy(full) # keep a copy
+full_flat = np.copy(full)
 
-## Load truncation inds
+## Load truncation inds 
 trunc = pd.read_csv(trunc_file_name, sep="\t", header=None).to_numpy().astype(np.int16)
 
-## Check that truncation inds are not NaN
+# put truncated values in matrix for plotting later. initialize matrices. 
+truncated_data = np.full_like(full, np.nan)
+# put hard shoreline values in matrix for plotting later
+hardshore_data = np.full_like(full, np.nan)
+hardshore_data_flat = np.full_like(full, np.nan)
+hardshore_data_flat_truncated = np.full_like(full, np.nan)
+
+## Check that truncation inds are not just NaN, if so skip that file. No truncation
 if np.product(trunc.shape) == 1 and trunc.flatten()[0] is np.nan:
     
     raise ValueError("Nothing to truncate")
@@ -34,33 +55,45 @@ if np.product(trunc.shape) == 1 and trunc.flatten()[0] is np.nan:
 
 trunc[:, 2] -= 1
 
-## Truncate relevant stuff
+#Truncate relevant stuff. Based on trunctaion file, remove duplicate data due to seawall 
+#     construction or other hard structure by setting those data to NaN 
+#     NOTE: This does not change the size of the matrix (doesn't remove 
+#           any rows or columns), it only replaces the duplicate data with NaNs
 
 for trans_start, trans_end, col_start, col_end in trunc:
     # Note that trans end is inclusive, but col end is not
     
     print(trans_start, trans_end, col_start, col_end)
     
-    row_start = np.where(full[:, 0] == trans_start)[0][0]
-    row_end = np.where(full[:, 0] == trans_end)[0][0] + 1
+    row_start = np.where(full[:, 0] == trans_start)[0][0] # find the row index that matches the trans_start
+    row_end = np.where(full[:, 0] == trans_end)[0][0] + 1  # find the row index that matches the trans_end + 1 (because need inclusivity)
     
     ## Since the first column (transect number) needs to be ignored
-    col_start += 1 # We don't actually need column start at all!
+    col_start += 1 # We don't actually need column start at all!? It seems trunc file ALWAYS has third column starting at index 1 (to keep)
+    col_end_og = col_end
     col_end += 1 
-        
+    
+    #keep_cols = range(col_start,col_end,1)
     ## Fill data
-    
-    if FILL_WITH_NANS:
-        
-        full[row_start:row_end, col_end:] = np.nan
-        
-    else:
-        
-        full[row_start:row_end, col_end:] = full[row_start:row_end, col_end-1].reshape((row_end-row_start, 1))
-        
-    print(full[row_start:row_end])
+
+    full[row_start:row_end, col_end:] = np.nan #if assuming third column is always 1, this is same as data_trunc #DATA_TRUNC.. #full[row_start:row_end, ~keep_cols] = np.nan
+    full_flat[row_start:row_end, col_end:] = full[row_start:row_end, col_end-1].reshape((row_end-row_start, 1)) 
     
     
     
+# put truncated values in matrix for plotting later. initialize matrices. 
+hardshore_data = np.full_like(full, np.nan) #All nans except col_end value, and Nan'd values (full)
+truncated_data = np.full_like(full, np.nan) #All nans except Nan'd values (full)
+hardshore_data_flat = np.full_like(full, np.nan) #All nans except col_end value, and Nan'd values  (full_flat)
+hardshore_data_flat_truncated = np.full_like(full, np.nan) #All nans except Nan'd values (full_flat)
     
+    
+#print(full[row_start:row_end])
+
+
+bools_full = ~np.isclose(full, full_original, equal_nan=True) #tracks the changed values between the two data arrays after the truncations. True if changed
+bools_full_flat =  ~np.isclose(full_flat, full_original, equal_nan=True)
+# truncated_data = np.copy(full).astype(float)
+# truncated_data[~bools_full] = np.nan
+# hardshore_data_flat = np.copy(full).astype(float);
 
